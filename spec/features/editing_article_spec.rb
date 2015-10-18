8889965a45917fae5d3954a48f7cc693f90bbe4e
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-18 10:12:41 +0200

    Implement editing articles.

---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.feature "Editing an article" do
+  before do
+    @article = Article.create(title: "First article", body: "Body of first article")
+  end
+
+  scenario "A user updates an article" do
+    visit "/"
+
+    click_link @article.title
+    click_link "Edit"
+
+    fill_in "Title", with: "Updated title of the article"
+    fill_in "Body", with: "Updated body of the article"
+
+    click_button "Update"
+
+    expect(page).to have_content("Article has been updated")
+    expect(page.current_path).to eq(article_path(@article))
+  end
+
+  scenario "A user fails to update an article" do
+    visit "/"
+
+    click_link @article.title
+    click_link "Edit"
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button "Update"
+
+    expect(page).to have_content("Article has not been updated")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
+end
