8e208d3de0a2e59392f30725d2e40da50bc35cd2
rubenmoya <rmoyarodriguez@gmail.com>
2015-10-19 20:21:46 +0200

    Add some restrictions.

--- spec/features/showing_articles_spec.rb
+++ spec/features/showing_articles_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.feature "Showing an article" do
   before do
-    @user = User.create(email: "user@example.com", password: "password")
-    login_as(@user)
-    @article = Article.create(title: "The first article", body: "Body of first article", user: @user)
+    @user1 = User.create(email: "user_one@example.com", password: "password")
+    @user2 = User.create(email: "user_two@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Body of first article", user: @user1)
   end
 
   scenario "Display individual article" do
@@ -16,5 +16,42 @@ RSpec.feature "Showing an article" do
     expect(page).to have_content(@article.body)
 
     expect(current_path).to eq(article_path(@article))
+  end
+
+  context "User IS loged in" do
+
+    it "Can edit/delete his article" do
+      login_as(@user1)
+
+      visit "/"
+
+      click_link @article.title
+
+      expect(page).to have_link("Edit")
+      expect(page).to have_link("Delete")
+    end
+
+    it "Can't edit/delete other user article" do
+      login_as(@user2)
+
+      visit "/"
+
+      click_link @article.title
+
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+
+  end
+
+  context "user IS NOT loged in" do
+    it "Can't edit/delete articles" do
+      visit "/"
+
+      click_link @article.title
+
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
   end
 end
